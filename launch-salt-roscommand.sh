70a29daf887d160a8c50ae54fcf3a271b07fc118
Josef Spillner <spio@tiguriner>
2016-06-07 16:15:02 +0200

    - simple introspection

--- launch-salt-roscommand.sh
+++ launch-salt-roscommand.sh
@@ -1,1 +1,3 @@
 salt -c master-config "*" ros.test
+salt -c master-config "*" ros.movetoposition 22.3 35.2
+salt -c master-config "*" ros.introspec
--- modules/ros.py
+++ modules/ros.py
@@ -1,2 +1,11 @@
 def test():
 	return True
+
+def movetoposition(x, y):
+	pass
+
+def introspec():
+	return filter(lambda x: x[0] != "_", globals())
+
+if __name__ == "__main__":
+	print(introspec())

0c784bdffb0df25172583e7d6f5279efe722ffd0
Josef Spillner <ecrp@ecrp>
2016-06-07 13:16:51 -0400

    - autogeneration of configuration files from templates to make the scripts more portable with regards to environment variables and paths - very basic readme file

--- launch-salt-master.sh
+++ launch-salt-master.sh
@@ -1,1 +1,2 @@
+sed -e "s,\$PWD,$PWD,;s,\$USER,$USER," config/master.in > config/master
 salt-master -c config -u $USER
--- launch-salt-minion.sh
+++ launch-salt-minion.sh
@@ -1,1 +1,2 @@
+sed -e "s,\$PWD,$PWD,;s,\$USER,$USER," config/minion.in > config/minion
 salt-minion -c config -u $USER

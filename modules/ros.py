5d09d7ae8e997b6d6e412975114e15447fdc49b3
Josef Spillner <ecrp@ecrp>
2016-06-08 16:10:34 -0400

    - new function to show details about each robot, in particular which functions exist - generic call function to invoke any of the functions, albeit still without parameters, and without results parsing - module side: generic entry barrier class to implement resource and method locks

--- modules/ros.py
+++ modules/ros.py
@@ -2,10 +2,32 @@ def test():
 	return True
 
 def movetoposition(x, y):
-	pass
+	return (x, y)
 
 def introspec():
-	return filter(lambda x: x[0] != "_", globals())
+	return filter(lambda x: x[0] != "_" and x[0].islower(), globals())
+
+class SafeGuard:
+	def __init__(self):
+		from functools import partial
+
+		self.funcs = {}
+		for func in introspec():
+			self.funcs[func] = globals()[func]
+			globals()[func] = partial(self.guard, func)
+
+	def guard(self, func, *args):
+		print("**safeguard entry [{:s}]**".format(func))
+		x = self.funcs[func](*args)
+		print("**safeguard exit [{:s}]**".format(func))
+		return x
+
+def safeguard():
+	global _sg
+	_sg = SafeGuard()
 
 if __name__ == "__main__":
+	safeguard()
+	print(test())
 	print(introspec())
+	print(movetoposition(1, 2))

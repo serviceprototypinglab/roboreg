72c7ffb0805d4f1c9b6f5d77f9a48e67b5eee24b
spio <spio@tiguriner>
2016-06-24 10:44:41 +0200

    - ros-ify the drive around app a bit; more to be included once it is deployed on the robot

--- apps/drivearound.py
+++ apps/drivearound.py
@@ -2,7 +2,10 @@ import os
 import rospy
 
 def drivearound():
-	pass
+	rospy.init_node("drivearound_robot")
+	print "ROS Node URI:", rospy.get_node_uri()
+	print "ROS Time:", rospy.get_rostime()
+	print "ROS_ROOT:", rospy.get_ros_root()
 
 if __name__ == "__main__":
 	drivearound()

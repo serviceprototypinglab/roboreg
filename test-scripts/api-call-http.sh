462db39c39758146ffcf0e5f1c9273f66eff5dd3
spio <spio@tiguriner>
2016-06-24 14:20:03 +0200

    - read API endpoint from the config file rather than hardcoding it

--- test-scripts/api-call-http.sh
+++ test-scripts/api-call-http.sh
@@ -1,11 +1,13 @@
 ## Note:
 ## This works, but only with salt-netapi.diff applied.
 
+api=`grep ^saltapi roboreg.ini | cut -d "=" -f 2 | xargs`
+
 tmp=`tempfile`
 
-http --headers localhost:8001/login username=ecrp password=s3cr3t eauth=auto | tee $tmp
+http --headers $api/login username=ecrp password=s3cr3t eauth=auto | tee $tmp
 token=`grep ^X-Auth-Token $tmp | cut -d ":" -f 2 | tr -d ' ' | tr -d '\r'`
 
 echo "TOKEN: [$token]"
 
-http localhost:8001 X-Auth-Token:$token client=local tgt='*' fun=ros.introspec
+http $api X-Auth-Token:$token client=local tgt='*' fun=ros.introspec

2f5e4812bfa98c77904ff0a9804e0cfa37518489
spio <spio@tiguriner>
2016-06-27 08:58:19 +0200

    - rename from ecrp to roboreg to make the naming less confusing for the beginning

--- test-scripts/api-call-http.sh
+++ test-scripts/api-call-http.sh
@@ -5,7 +5,7 @@ api=`grep ^saltapi roboreg.ini | cut -d "=" -f 2 | xargs`
 
 tmp=`tempfile`
 
-http --headers $api/login username=ecrp password=s3cr3t eauth=auto | tee $tmp
+http --headers $api/login username=roboreg password=s3cr3t eauth=auto | tee $tmp
 token=`grep ^X-Auth-Token $tmp | cut -d ":" -f 2 | tr -d ' ' | tr -d '\r'`
 
 echo "TOKEN: [$token]"
